a5acc84bb4fcbac6fd2620c94ea15ebc80c6dba3
wonpyo_kwon <wonpyo_kwon@tmax.co.kr>
2021-07-09 19:21:40 +0900

    클론 코딩 7장 완료 클론 코딩 7장까지 공부 완료.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 import './App.css';
+import logo from './logo.svg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
+    console.log(movies);
+    this.setState({ movies: movies,  // 변수 이름이 같으면 축약 가능 ex) {movies}
+                    isLoading: false, });
+  }
+  componentDidMount() {
+    // 영화 데이터 로딩!
+    this.getMovies();
+  }
+  componentDidUpdate() {
+  }
+  componentWillUnmount() {
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return(
+      <section className="container">
+        {isLoading 
+        ? <div className="loader">
+            <img src={logo} className="App-logo" alt="logo" />
+            <span className="loader__text">영화 목록을 불러오는 중입니다...</span>
+          </div>
+        : <div className="movies">
+          {movies.map(movie => {
+            return (
+            <Movie 
+              key={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+              genres={movie.genres}
+            />
+            );
+          })}
+          </div>
+        }
+      </section>
+    );
+  }
 }
 
 export default App;
